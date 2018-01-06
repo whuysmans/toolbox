b698eb9fc01a55983fd1204c944769ebec403730
dewerner <werner.huysmans@gmail.com>
2018-01-06 17:56:49 +0100

    now testing without server check

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,5 @@
 export default function (context) {
-  if (context.isServer) {
-    if (!context.store.getters.isAuthenticated) {
-      return context.redirect('/login')
-    }
+  if (!context.store.getters.isAuthenticated) {
+    return context.redirect('/login')
   }
 }
