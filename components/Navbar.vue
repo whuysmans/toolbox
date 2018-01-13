ca4ce47666c9b856faa67cd5a782270459e8cd71
dewerner <werner.huysmans@gmail.com>
2018-01-13 11:00:05 +0100

    minor ux changes

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -10,14 +10,14 @@
         <div class="navbar-menu" :class="{'is-active': isActive}">
           <div class="navbar-start"></div>
           <div class="navbar-end" style="margin-right: 2em;">
-            <div class="navbar-item is-hoverable">
-              <a  style="color: black;" href="https://cipt.be/toolbox/topic/welkom-op-het-forum">Ideeënforum</a>
-            </div>
             <div v-if="parent === 'biblio'" class="navbar-item is-hoverable">
               <nuxt-link style="color: black;" to="/">Home</nuxt-link>
             </div>
             <div v-else class="navbar-item is-hoverable">
               <nuxt-link style="color: black;" to="/bibliografie">Bibliografie</nuxt-link>
+            </div>
+            <div class="navbar-item is-hoverable">
+              <a  style="color: black;" href="https://cipt.be/toolbox/topic/welkom-op-het-forum">Ideeënforum</a>
             </div>
           </div>
         </div>
--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'toolbox2',
+    title: 'Formatief Evalueren',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
